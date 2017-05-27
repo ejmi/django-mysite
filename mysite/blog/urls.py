f956a7c4d3e31161099dbdba3eb9cbe2a4818ba0
emi <3godlewska@gmail.com>
2017-05-27 21:37:58 +0200

    reorganized views (blog)

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url, include
-from django.views.generic import ListView, DetailView
-from blog.models import Post
-
+from . import views
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:20],
-                                template_name="blog/blog.html")),
-    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Post,
-                                            template_name = "blog/post.html"))
-]
+    url(r'^$', views.ListView.as_view(), name='blog'),
+    url(r'^(?P<pk>\d+)$', views.DetailView.as_view(), name='post'),
+        ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+from .models import Post
+
+class ListView(generic.ListView):
+    model = Post
+    template_name = 'blog/blog.html'
+
+    def get_queryset(self):
+        return Post.objects.all().order_by("-date")[:25]
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post.html'
